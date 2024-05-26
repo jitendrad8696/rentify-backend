7cc5cbb46324731d523b2449ec1ac6db7e72984c
Jitendra Dhakar <Jitendrad869601@gmail.com>
2024-05-26 20:16:41 +0530

    first commit

--- src/middlewares/verify.token.js
+++ src/middlewares/verify.token.js
@@ -3,7 +3,10 @@ import { APIError } from "../utils/APIError.js";
 import { ACCESS_TOKEN_JWT } from "../config/index.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token || req.headers.Authorization?.split(" ")[1];
+  const token =
+    req.cookies.token ||
+    req.headers["authorization"]?.split(" ")[1] ||
+    req.headers.Authorization?.split(" ")[1];
 
   if (!token) {
     return next(new APIError(401, "No token provided."));
